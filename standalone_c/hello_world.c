9c21baa413d453453f5eec78076894a478ba7b06
tanneberger <github@tanneberger.me>
2024-11-26 20:44:41 +0100

    adding TODOs

--- standalone_c/hello_world.c
+++ standalone_c/hello_world.c
@@ -6,10 +6,17 @@ typedef struct {
                          // startup.
 } MyStartupTrigger;
 
+// TODO: there is now also a ShutdownTrigger
+
 typedef struct {
   Reactor super;
   Reaction reaction;
+  // TODO: there is a new shutdown reaction
+
   MyStartupTrigger startup;
+  // TODO: You just created a new shutdown trigger
+
+  //TODO: we have one more trigger and one more reaction
   Trigger *triggers[1];  // For storing pointers to all the triggers within this
                          // reactor.
   Reaction *reactions[1];  // For storing pointers to all the reactions within
@@ -18,6 +25,8 @@ typedef struct {
 
 // Reaction body
 void on_startup(Reaction *self) { printf("Hello, World!\n"); }
+// TODO: Your shutdown reaction body
+
 
 // The "environment" contains reaction queue, event queue, scheduler.
 Environment env;
@@ -27,6 +36,8 @@ MyReactor reactor;
 
 int main() {
   Environment_ctor(&env, &reactor.super);
+
+  // TODO: the length of those reactions and triggers array changed!
   Reactor_ctor(&reactor.super, "MyReactor", &env, NULL, NULL, 0,
                reactor.reactions, 1, reactor.triggers, 1);
 
@@ -34,11 +45,14 @@ int main() {
   reactor.startup.effects[0] = &reactor.reaction;
   BuiltinTrigger_ctor(&reactor.startup.super, TRIG_STARTUP, &reactor.super,
                       reactor.startup.effects, 1, NULL, 0);
+  
+  // TODO: You need to configure and construct the Shutdown Trigger now!
 
   // Configure the reaction
   Reaction_ctor(&reactor.reaction, &reactor.super, on_startup, NULL, 0, 0);
-
+  
   // Configure the reactor
+  // TODO: we now have one more reaction and one more trigger!
   reactor.triggers[0] = &reactor.startup.super.super;
   reactor.reactions[0] = &reactor.reaction;
 
